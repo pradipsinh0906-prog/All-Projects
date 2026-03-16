4c91f359176b5070d86cd2a9ee200a31d8ec2ce2
Vivek Barevadiya <barevadiyavivek48@gmail.com>
2026-03-16 11:22:29 +0530

    SMTP email configration update regrading for the website.

--- reels_project/reels_project/settings.py
+++ reels_project/reels_project/settings.py
@@ -133,5 +133,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'barevadiyavivek48@gmail.com'
-EMAIL_HOST_PASSWORD = 'rwwudvkmwyureyey'
+EMAIL_HOST_USER = 'instadownloader14@gmail.com'
+EMAIL_HOST_PASSWORD = 'prljdbxnibxdnmjz'
